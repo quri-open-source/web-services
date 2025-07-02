82036b810f8abb62973103f3592b4a997a1f3e0e
Neil <98752609+Neilcuri@users.noreply.github.com>
2025-07-02 18:10:38 -0500

    fix: refactor shopping Cart

--- src/main/java/quri/teelab/api/teelab/orderprocessing/domain/model/queries/GetAllItemsInShoppingCartQuery.java
+++
@@ -1,9 +0,0 @@
-package quri.teelab.api.teelab.orderprocessing.domain.model.queries;
-
-import java.util.UUID;
-
-public record GetAllItemsInShoppingCartQuery(UUID shoppingCartId) {
-    public GetAllItemsInShoppingCartQuery {
-        if (shoppingCartId == null) throw new IllegalArgumentException("shoppingCartId cannot be null");
-    }
-}
