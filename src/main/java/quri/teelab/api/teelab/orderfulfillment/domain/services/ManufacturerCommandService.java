1bb4b134aca5d05aacd9ffc8500c5d28dfc425d5
zGIKS <166082577+zGIKS@users.noreply.github.com>
2025-07-02 19:52:47 -0500

    feat: add handle method to ManufacturerCommandService for CreateManufacturerCommand

--- src/main/java/quri/teelab/api/teelab/orderfulfillment/domain/services/ManufacturerCommandService.java
+++ src/main/java/quri/teelab/api/teelab/orderfulfillment/domain/services/ManufacturerCommandService.java
@@ -1,4 +1,10 @@
 package quri.teelab.api.teelab.orderfulfillment.domain.services;
 
+import quri.teelab.api.teelab.orderfulfillment.domain.model.aggregates.Manufacturer;
+import quri.teelab.api.teelab.orderfulfillment.domain.model.commands.CreateManufacturerCommand;
+
+import java.util.Optional;
+
 public interface ManufacturerCommandService {
+    Optional<Manufacturer> handle(CreateManufacturerCommand command);
 }
