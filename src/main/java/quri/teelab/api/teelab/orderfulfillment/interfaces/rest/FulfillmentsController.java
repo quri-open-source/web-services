14318661b9f6f0d86510ebce71d47f48f597e3c9
Brayan Roberto Ccarita Cruz <hallinxsl7@gmail.com>
2025-07-03 22:39:13 -0500

    update: update fulfillment controller

--- src/main/java/quri/teelab/api/teelab/orderfulfillment/interfaces/rest/FulfillmentsController.java
+++ src/main/java/quri/teelab/api/teelab/orderfulfillment/interfaces/rest/FulfillmentsController.java
@@ -75,7 +75,11 @@ public class FulfillmentsController {
 
     @PostMapping
     @Operation(summary = "Create fulfillment", description = "Create a new fulfillment with the provided details")
-    @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "Fulfillment created successfully"), @ApiResponse(responseCode = "400", description = "Invalid input data"), @ApiResponse(responseCode = "404", description = "Fulfillment not found")})
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Fulfillment created successfully"), 
+        @ApiResponse(responseCode = "400", description = "Invalid input data"), 
+        @ApiResponse(responseCode = "404", description = "Fulfillment not found")
+    })
     public ResponseEntity<FulfillmentResource> createFulfillment(@RequestBody CreateFulfillmentResource resource) {
         var createFulfillmentCommand = CreateFulfillmentCommandFromResourceAssembler.toCommandFromResource(resource);
         var fulfillmentId = fulfillmentCommandService.handle(createFulfillmentCommand);
